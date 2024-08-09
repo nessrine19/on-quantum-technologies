eb3eb2bd1ad0d100b67786482ca08979ce6c6bce
nesrine <nesrineabdelhak7@gmail.com>
2024-08-09 13:02:31 +0100

    update to qiskit 1.0

--- content/lib/get_results.py
+++ content/lib/get_results.py
@@ -1,7 +1,9 @@
-from qiskit import Aer, execute
+# from qiskit import Aer
+from qiskit_aer import Aer
 
 def get_results(qc, shots=1000):
-    job = execute(qc, Aer.get_backend("qasm_simulator"), shots=shots)
-    counts = job.result().get_counts(qc)
-    
+
+    backend = Aer.get_backend('statevector_simulator')
+    result = backend.run(qc.decompose(reps=6), shots=shots).result()   
+    counts = result.get_counts()   
     return counts
--- content/lib/get_statevector.py
+++ content/lib/get_statevector.py
@@ -1,9 +1,12 @@
-from qiskit import Aer, execute
+from qiskit import transpile
+from qiskit_aer import AerSimulator
+from qiskit_aer import Aer
 
 def get_statevector(circuit):
-    be_sv = Aer.get_backend("statevector_simulator")
-    job = execute(circuit, be_sv, optimization_level = 0)
+    simulator = Aer.get_backend('statevector_simulator')
+    result = simulator.run(circuit.decompose(reps=6)).result()
+    statevector=result.get_statevector(circuit)
     precision = 3
-    current_quantum_state = job.result().get_statevector(circuit, precision).data
+    current_quantum_state = [round(val, precision) for val in statevector]
     
     return current_quantum_state
